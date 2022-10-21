3301ca70a746fa38d512d2000f404a47d1d3de73
Josh Gumerove <jgumerove@kaufmanrossin.com>
2022-10-20 21:17:01 -0400

    add migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord #note how it inherits from Application Record (wh
     validates :name, presence: true, length: {maximum: 50}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i #note how it is completely fine to declare a variable above a validatioin
     validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+    has_secure_password
 end
 
 #note how we confirmed that it inherits from active record base in the terminal ---> ApplicationRecord < ActiveRecord::Base
@@ -15,3 +16,6 @@ end
 #note how we can create a custom message
 #note when rails will infer something to be true
 #compare password (hashed) to the result stored in the database
+#can authenticate users without storing the password themselves
+#note what is meant by password_digest -- neads to be added as an attribute for has_secure_password to work
+#note the syntax for creating a migration rails g migration add_password_digest_to_users password_digest:string
