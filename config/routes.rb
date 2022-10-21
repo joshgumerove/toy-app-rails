42372f132b49eeca610fcf4b416e0815befdc01a
Josh Gumerove <jgumerove@kaufmanrossin.com>
2022-10-21 17:22:27 -0400

    add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'static_pages/help'
+  get 'static_pages/about'
   resources :microposts
   # :users -- this is an example of a symbol
   resources :users
