32f403c11f0d4bcb71d1a70957c13dd82db4f2e3
Josh Gumerove <jgumerove@kaufmanrossin.com>
2022-10-27 08:55:52 -0400

    templating

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,14 @@
 class StaticPagesController < ApplicationController
   def home
+    # puts "what is it #{Rails.env}" not how we can find out the current rails environment
   end
 
   def help
   end
 
   def about
+  end
+
+  def contact
   end
 end
